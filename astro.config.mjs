5055c035890624e1f95c19ae4c29c656f9bc5983
Laba Subagia <labasubagia22@gmail.com>
2024-01-06 22:44:08 +0800

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://labasubagia.github.io',
   integrations: [tailwind(), svelte()],
 })
